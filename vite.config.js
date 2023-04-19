465472be4f22fbd47d171042c9fc2c830dea1b1a
Magnus 'Bagzy' Thor <magnus.thor74@gmail.com>
2023-04-19 16:07:58 +0200

    relative path fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '', 
     plugins: [vue()],
     resolve: {
         alias: {
